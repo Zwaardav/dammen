752ea2f3b614fb763c27eda161f973eb798b0f10
Zwaardav <d.korsman@student.ru.nl>
2016-04-14 10:59:09 +0200

    BordActivity, knop om naar dambord te gaan

---
+++ app/src/main/java/haakjeopenen/dammen/BordActivity.java
@@ -0,0 +1,13 @@
+package haakjeopenen.dammen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BordActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bord);
+	}
+}
