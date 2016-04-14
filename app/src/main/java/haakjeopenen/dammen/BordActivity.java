21608ac4a6db813120ce18da53bdd307e2507b8e
Zwaardav <d.korsman@student.ru.nl>
2016-04-14 11:50:43 +0200

    Plaatje van een dambord, gefaald grid

--- app/src/main/java/haakjeopenen/dammen/BordActivity.java
+++ app/src/main/java/haakjeopenen/dammen/BordActivity.java
@@ -2,6 +2,8 @@ package haakjeopenen.dammen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class BordActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class BordActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bord);
+
+		/*
+		ImageView iv = new ImageView(this);
+		iv.setImageResource(R.drawable.zwart);
+		RelativeLayout rl = (RelativeLayout) findViewById(R.id.bordimage);
+		RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.WRAP_CONTENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT);
+		lp.addRule(RelativeLayout.BELOW, R.id.bordimage);
+		lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+		rl.addView(iv, lp);
+		*/
 	}
 }
