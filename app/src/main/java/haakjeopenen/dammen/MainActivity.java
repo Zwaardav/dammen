2dc83fad1c76f6816a69b5a7dc6ead1768722f17
Wietze Mulder <wietzem@gmail.com>
2016-04-26 12:14:06 +0200

    Layout changes

--- app/src/main/java/haakjeopenen/dammen/MainActivity.java
+++ app/src/main/java/haakjeopenen/dammen/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		/*
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 						.setAction("Action", null).show();
 			}
 		});
+		*/
 	}
 
 	@Override
